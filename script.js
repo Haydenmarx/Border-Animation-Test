902aecbebee16e4c24097b9560adf27bf32e5b6f
Hayden Marx <haydenmarx@gmail.com>
2017-05-11 17:12:37 -0500

    Bug fix to make the border continuous.

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ function borderAnimation (section){
   if (section) {
     container = section
   }
-    $(container).children('.box-container').children('.animated').each(function(){
-      $(this).animate({
-        top: 0,
-        left: 0
-      }, timer)
-      timer = timer+200;
-    })
+  let boxTop = $(container).find('.box-top');
+  let boxRight = $(container).find('.box-right');
+  $(container).children('.box-container').children('.animated').each(function(){
+    $(this).animate({
+      top: 0,
+      left: 0
+    }, timer)
+    timer = timer+200;
+  })
+  boxTop.promise().done(function() {
+    boxRight.css('border-top', '3px solid black');
+  })
 }
 
 function killButtons (parent){
